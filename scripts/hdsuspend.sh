ddf670b5e61349d1b19a10935ec47377865f89a3
Mathias Rüdiger <ruediger@blueboot.org>
2016-11-16 08:31:59 +0100

    updated hdsuspend settings

--- scripts/hdsuspend.sh
+++ scripts/hdsuspend.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+sudo_install hdparm
+
 if [[ `hostname` == clementine ]] ; then
   sudo cp $WORKDIR/templates/hdsuspend.service /etc/systemd/system/hdsuspend.service
   sudo systemctl enable hdsuspend.service
