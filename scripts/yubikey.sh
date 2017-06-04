d83f29a7c53f494ef94d9aa8ce6992d1e8d8b943
Mathias Rüdiger <ruediger@blueboot.org>
2017-06-04 14:37:32 +0200

    update yubi-key settings

--- scripts/yubikey.sh
+++ scripts/yubikey.sh
@@ -1,3 +1,6 @@
-#!/usr/bin/env
+#!/usr/bin/env bash
 
 echo 'ACTION=="add", SUBSYSTEM=="usb", ENV{ID_VENDOR_ID}=="1050", ENV{ID_MODEL_ID}=="0116|0111", MODE="660", GROUP="wheel"' | sudo tee /etc/udev/rules.d/99-yubikey.rules
+
+on_arch && sudo pacman -S --noconfirm --needed pcsc-tools ccid libusb-compat
+sudo pacman -S --noconfirm --needed pcsc-tools ccid libusb-compat
