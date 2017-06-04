69a3970037e4c88ad12d96ab0ba7f8e9f88b3c97
Mathias Rüdiger <ruediger@blueboot.org>
2017-06-04 15:27:08 +0200

    add git-lfs

--- scripts/git.sh
+++ scripts/git.sh
@@ -8,3 +8,9 @@ git config --global push.default "simple"
 git config --global url."git@github.com:brainly".insteadOf "https://github.com/brainly"
 git config --global url."git@github.com:fromatob".insteadOf "https://github.com/fromatob"
 git config --global core.editor "emacs -nw"
+
+mkdir -p $HOME/bin
+
+pushd $HOME/bin
+curl -sL https://github.com/git-lfs/git-lfs/releases/download/v2.1.1/git-lfs-linux-amd64-2.1.1.tar.gz | tar xz git-lfs-2.1.1/git-lfs --strip 1
+popd
