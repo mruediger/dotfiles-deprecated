f4bd35816cb0bf375b615aae912408117ae7453d
Mathias Rüdiger <ruediger@blueboot.org>
2017-08-05 22:33:59 +0200

    identify default sink on all machines

--- templates/awesome/pulseaudio.lua
+++ templates/awesome/pulseaudio.lua
@@ -2,18 +2,16 @@ local awful = require("awful")
 
 local pulseaudio = {}
 
-local default_sink = "alsa_output.pci-0000_00_1f.3.analog-stereo"
-
 function pulseaudio:VolumeUp()
-   awful.spawn('pactl set-sink-volume ' .. default_sink .. ' +5%', false)
+   awful.spawn('pactl set-sink-volume 0 +5%', false)
 end
 
 function pulseaudio:VolumeDown()
-   awful.spawn('pactl set-sink-volume ' .. default_sink .. ' -5%', false)
+   awful.spawn('pactl set-sink-volume 0 -5%', false)
 end
 
 function pulseaudio:ToggleMute()
-   awful.spawn('pactl set-sink-mute ' .. default_sink .. ' toggle', false)
+   awful.spawn('pactl set-sink-mute 0 toggle', false)
 end
 
 return pulseaudio
