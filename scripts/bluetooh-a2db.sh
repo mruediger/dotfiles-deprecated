c1f25ac5c0bda0aa68dc86fab9fbb1979ea9907a
Mathias Rüdiger <ruediger@blueboot.org>
2017-09-25 10:29:15 +0200

    update bluetooth audio script

--- scripts/bluetooh-a2db.sh
+++ scripts/bluetooh-a2db.sh
@@ -2,8 +2,11 @@
 
 sudo_install pulseaudio-bluetooth
 
-sudo cp templates/audio.conf /etc/bluetooth/audio.conf
+sudo cp $WORKDIR/templates/audio.conf /etc/bluetooth/audio.conf
 
 #bluetoothctl power on
 #bluetoothctl discoverable
 #bluetoothctl trust
+
+
+sudo sed -i 's/#AutoEnable=false/AutoEnable=true/' /etc/bluetooth/main.conf
