50fbe808d8a1c0798fc4a68d89c9f7682c25d39e
Mathias Rüdiger <ruediger@blueboot.org>
2017-12-21 16:49:53 +0100

    some updates

--- scripts/awesome.sh
+++ scripts/awesome.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-sudo_install awesome vicious i3lock
+sudo_install awesome vicious i3lock rxvt-unicode
 
-ln -s $WORKDIR/templates/awesome $HOME/.config/awesome 
+mkdir -p $HOME/.config
+
+ln -s $WORKDIR/templates/awesome $HOME/.config/awesome
 
 mkdir -p $HOME/bin
 cp templates/toggle-touchpad $HOME/bin
