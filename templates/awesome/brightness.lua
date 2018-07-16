37d234cf2ae4a0ac8b8ed8b8c3225eb459fdcb8b
Mathias Rüdiger <ruediger@blueboot.org>
2018-07-16 10:55:20 +0200

    update brightness.lua to use brightnessctl

--- templates/awesome/brightness.lua
+++ templates/awesome/brightness.lua
@@ -3,24 +3,12 @@ local naughty = require("naughty")
 
 local brightness = {}
 
-local sysfile = "/sys/class/backlight/intel_backlight/brightness"
-
 function brightness:up()
-   awful.spawn.with_line_callback('cat ' .. sysfile, {
-     stdout = function(line)
-        brtns = tonumber(line) + 100
-        awful.spawn.with_shell('echo ' .. brtns .. ' > ' .. sysfile, false)        
-     end,
-   })
+   awful.spawn("brightnessctl s +5%", false)
 end
 
 function brightness:down()
-   awful.spawn.with_line_callback('cat ' .. sysfile, {
-     stdout = function(line)
-        brtns = tonumber(line) - 100
-        awful.spawn.with_shell('echo ' .. brtns .. ' > ' .. sysfile, false)        
-     end,
-   })
+   awful.spawn("brightnessctl s 5%-", false)
 end
 
 return brightness
