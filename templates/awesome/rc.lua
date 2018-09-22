1036a236c524538c4ef39d4ddaec024589c812b6
Mathias Rüdiger <ruediger@blueboot.org>
2018-09-22 15:05:28 +0200

    change terminal to termite

--- templates/awesome/rc.lua
+++ templates/awesome/rc.lua
@@ -50,7 +50,7 @@ end
 beautiful.init(awful.util.get_configuration_dir() .. "theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "urxvt"
+terminal = "termite"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
