423985eec670c29eb41d8eb2e3dc561515a325c8
Mathias Rüdiger <ruediger@blueboot.org>
2018-12-27 19:17:09 +0100

    finer brightness controll

--- templates/awesome/brightness.lua
+++ templates/awesome/brightness.lua
@@ -4,11 +4,11 @@ local naughty = require("naughty")
 local brightness = {}
 
 function brightness:up()
-   awful.spawn("brightnessctl s +5%", false)
+   awful.spawn("brightnessctl s +2", false)
 end
 
 function brightness:down()
-   awful.spawn("brightnessctl s 5%-", false)
+   awful.spawn("brightnessctl s 2-", false)
 end
 
 return brightness
